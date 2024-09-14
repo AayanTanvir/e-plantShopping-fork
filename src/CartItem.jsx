e9642431a413a979cddf38b25accafa8cf211f84
AayanTanvir <k98778376@gmail.com>
2024-09-14 09:48:19 -0400

    added more stuff

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -6,30 +6,40 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
+  var totalItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => total + item.quantity * item.cost, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality  to be added for future references');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.quantity + 1));
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity > 1){
+        dispatch(updateQuantity(item.quantity - 1));
+   } else if(item.quantity === 1){
+        dispatch(removeItem(item));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * item.cost;
   };
 
   return (
